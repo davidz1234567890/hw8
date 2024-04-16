f2f35a2f37c80a11e55429cccf8b0d94a918f912
David Zhang <dzhang2@ece001.ece.local.cmu.edu>
2024-04-16 02:40:26 -0400

    fixed 1a

--- hw8prob1a.asm
+++ hw8prob1a.asm
@@ -1,29 +1,45 @@
-      ; this program takes in 2 numbers
-      ; the first at address $1000
-      ; the second at address $1002
-      ; it keeps adding 1 to the first number
-      ; and keeps subtracting 1 from the second number
-      ; until the first number is greater than or equal to the second number
-      ; if the first number is already greater than or equal to the second,
-      ; it returns immediately
-      ; the ending values of the first and second number are stored in
-      ; R2 and R3 respectively
+      ; reverses an array of length 5
 
       .ORG $000
       BRA $200
 
-      .ORG $1000
-      .DW $0 ; first number
-      .DW $8 ; second number
+      .ORG $100
+ARR   .DW $9
+      .DW $8
+      .DW $E
+      .DW $F
+      .DW $0
+
 
       .ORG $200
-START LW R2, R0, $1000 ; loads first number into R2
-      LW R3, R0, $1002 ; loads second number into R3
-      LI R1, $1 ; R1 <- 1, this is for subtraction
-LOOP  SLT R0, R3, R2 ; computes second number - first number
-                     ; and discards the result
-      BRNZ FINISH ; exits if second number <= first number
-      ADDI R2, R2, $1 ; first number <- first number + $1
-      SUB R3, R3, R1 ; second <- second - R1 where R1 <- $1
-      BRA LOOP ; goes back to the loop
-FINISH  STOP
+START LI R1, $0 ; i <- 0
+
+
+LOOP  SLLI R2, R1, $1 ; computes i*2 which is the offset
+                      ; of array[i] from beginning of array
+      LW R3, R2, $100 ; load array[i] into R3
+      SW R0, R3, $500 ; m[$500] = array[i]
+      LI R3, $1 ; R3 <- 1 for subtracting purposes
+      LI R4, $5 ; R4 <- 5 for subtracting purposes
+      SUB R6, R4, R1
+      SUB R6, R6, R3 ; this line and above compute 5 - i - 1
+      SLLI R6, R6, $1 ; computes (5-i-1)*2
+                      ; which is the offset of array[5-i-1]
+                      ; from the beginning of the array
+      LW R3, R6, $100 ; load array[5-i-1] into R3
+      SW R2, R3, $100 ; R2 still contains offset of array[i] from
+                      ; beginning of array
+                      ; so this operation does
+                      ; array[i] = array[5-i-1]
+      LW R3, R0, $500 ; m[$500] contains array[i]
+                      ; R3 <- array[i]
+      SW R6, R3, $100 ; R6 still contains offset of array[5-i-1]
+                      ; from beginning of array
+                      ; so this operation does
+                      ; array[5-i-1] = array[i]
+      ADDI R1, R1, $1 ; i <- i + $1
+      SRAI R7, R4, $1 ; divides 5 by 2
+      SLT R0, R1, R7 ; computes i - 5/2
+      BRNZ LOOP ; continues loop if i <= 5/2
+      BRA END ; terminates loop if i > 5/2
+END   STOP
